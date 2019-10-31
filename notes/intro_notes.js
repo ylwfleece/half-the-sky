6e73414bddde99beb156f668afdd613514e4bce2
Max Lettenberger <maxlettenberger@gmail.com>
2019-10-30 22:04:42 -0400

    Finish notes for intro

--- notes/intro_notes.js
+++ notes/intro_notes.js
@@ -1,56 +1,139 @@
-
-
+// quotes for the intro
+//
 let introQuotes = {
 
-"pgXIV" : 
+    "xiv":
 
-[
+        [
 
-"one chinese family-planning official, Li Honggui, explained it this way: ‘If a boy gets sick, the parents may send him to the hospital at once. But if a girl gets sick, the parents may say to themselves, ‘Well, let’s see how she is tomorrow.’’The result is that as many infant girls die unnecessarily every week in China as protesters died in the one incident at Tiananmen." 
+            "one chinese family-planning official, Li Honggui, explained it this way: ‘If a boy gets sick, the parents may send him to the hospital at once. But if a girl gets sick, the parents may say to themselves, ‘Well, let’s see how she is tomorrow.’’The result is that as many infant girls die unnecessarily every week in China as protesters died in the one incident at Tiananmen."
 
-,
+            ,
 
-"In india, a ‘bride burning’-- to punish a woman for an inadequate dowry or to eliminate her so a man can remarry--takes place approximately once every two hours." 
+            "In india, a ‘bride burning’-- to punish a woman for an inadequate dowry or to eliminate her so a man can remarry--takes place approximately once every two hours."
 
-,
+            ,
 
-"In the twin cities of islamabad and rawalpindi, Pakistan, five thousand women and girls have been doused in kerosene and set alight by family members or in-laws--or, perhaps worse, been seared with acid--for perceived disobedience just in the last nine years." 
+            "In the twin cities of islamabad and rawalpindi, Pakistan, five thousand women and girls have been doused in kerosene and set alight by family members or in-laws--or, perhaps worse, been seared with acid--for perceived disobedience just in the last nine years."
 
-,
+            ,
 
-"A tiny portion of US foreign aid is specifically targeted to women and girls." 
+            "A tiny portion of US foreign aid is specifically targeted to women and girls."
 
-]
+        ]
 
-,
+    ,
 
-"pgXV": 
+    "xv":
 
-[
+        [
 
-"In normal circumstances women live longer than men, and so there are more females than males in much of the world." 
+            "In normal circumstances women live longer than men, and so there are more females than males in much of the world."
 
-,
+            ,
 
-"China has 107 males for every 100 females in its overall population (and an even greater disproportion of newborns), India has 108, and Pakistan has 111. This has nothing to do with biology, and indeed the state of Kerala in the southwest of India, which has championed female education and equality, has the same excess of  females that exists in the United States" 
+            "China has 107 males for every 100 females in its overall population (and an even greater disproportion of newborns), India has 108, and Pakistan has 111. This has nothing to do with biology, and indeed the state of Kerala in the southwest of India, which has championed female education and equality, has the same excess of  females that exists in the United States"
 
-,
+            ,
 
-"The implication of the sex ratios, Professor Sen found, is that about 107 million females are missing from the globe today. Follow-up studies have calculated the number slightly differently, deriving alternative figures for ‘missing women’ of between 60 million and 101 million. Every year, at least another 2 million girls worldwide disappear because of gender discrimination." 
+            "The implication of the sex ratios, Professor Sen found, is that about 107 million females are missing from the globe today. Follow-up studies have calculated the number slightly differently, deriving alternative figures for ‘missing women’ of between 60 million and 101 million. Every year, at least another 2 million girls worldwide disappear because of gender discrimination."
 
-,
+            ,
 
-"In America, millions of women and girls face beatings or other violence from their husbands or boyfriends, and more than one in six undergoes rape or attempted rape at some point in her life, according to the National Violence Against Women survey." 
+            "In America, millions of women and girls face beatings or other violence from their husbands or boyfriends, and more than one in six undergoes rape or attempted rape at some point in her life, according to the National Violence Against Women survey."
 
-,
+        ]
 
-""
+    ,
+
+    "xvi":
+
+        [
+
+            "Teenage runaways are beaten, threatened, and branded (with tattoos) by pimps in American cities, and thousands of foreign women are trafficked into the United States as well. Still, in poor countries gender discrimination is often lethal in a way that it usually is not in America. In India, for example, mothers are less likely to take their daughters to be vaccinated than their sons--that alone accounts for one fifth of India's missing females--while studies ahve found that, on average, girls are brought tot he hospital only when they are sicker than boys taken to the hospiatl. All told, girls in India from one to five years of age are 50 percent more likely to die than boys the same age. The best estimate is that a little Indian girl dies from discrimination every four minutes."
+
+            ,
+
+            "To prevent sex-selective abortion, China and India now bar doctors and ultrasound technicians from telling a pregnant woman the sex of her fetus. Yet that is a flawed solution. Research shows that when parents are banned from selectively aborting female fetuses, more of their daughters die as infants. Mothers do not deliberately dispatch infant girls they are obligated to give birth to, but they are lackadaisical in caring for them. A development economist at Brown University, Nancy Qian, quantified the wrenching trade-off: On average, the deaths of fifteen infant girls can be avoided by allowing one hundred female fetuses to be selectively aborted."
+
+            ,
+
+            "It appears that more girls have been killed in the last fifty years, precisely because they were girls, than men were killed in all the battles of the twentieth century."
+
+            ,
+
+            "In the nineteenth century, the central moral challenge was slavery. In the twentieth century, it was the battle against totalitarianism. We believe that in this centruy the paramount moral challenge will be the struggle for gender equality around the world."
+
+        ]
+
+    ,
+
+    "xix":
+
+        [
+
+            "Eighty percent of the employees on the assembly lines in coastal China are female, and the proportion across the manufacturing belt of East Asia is at least 70 percent."
+
+            ,
+
+            "The economic expansion of Asia was, in large part, an outgrowth of the economic empowerment of women. 'They have smaller fingers, so they're better at stitching,' the manager of a purse factory explained to us. 'They're obedient and work harder than men,' said the head of a toy factory. 'And we can pay them less'"
+
+            ,
+
+            "These countries took young women who previously had contributed negligibly to gross national product (GNP) and injected them into the formal economy, hugely increasing the labor force. The basic formula was to ease repression, educate girls as well as boys, give the girls the freedom to move to the cities and take factory jobs, and then benefit from a demographic dividend as they delayed marriage and reduced childbearing. The women meanwhile financed the education of younger relatives, and saved enough of their pay to boost national savings rates."
+
+        ]
+
+    ,
+
+    "xx":
+
+        [
+            "In Bangladesh, Muhammad Yunus developed microfinance at the Grameen Bank and targeted women borrowers--eventually winning a Nobel Peace Prize for the economic and social impact of his work. Another Bangladeshi group, BRAC, the largest antipoverty organization in the world, worked with the poorest women to save lives and raise incomes--and Grameen and BRAC made the aid world increasingly see women not just as potential beneficiaries of their work, but as agents of it."
+
+            ,
+
+            "UNICEF issued a major report arguing that gender equality yields a 'double dividend' by elevating not only women but also their children and communities. The United Nations Dvelopment Programme (UNDP) summed up the mounting research this way: 'Women's empowerment helps raise economic productivity and reduce infant morality. It contributes to improved health and nutrition. It increases the chances of education for the next generation."
+
+            ,
+
+            "'Women are the key to ending hunger in Africa,' declared the Hunger Project."
+
+            ,
+
+            "'French foreign minister Bernard Kouchner, who founded Doctors Without Borders, bluntly declared of development: 'Progress is achieved through women.'"
+
+        ]
+
+    ,
+
+    "xxi":
+
+        [
+
+            "'Gender inequality hurts economic growth,' Goldman Sachs concluded in a 2008 research report that emphasized how much developing countries could improve their economic performance by educating girls."
+
+            ,
+
+            "Concerns about terrorism after the 9/11 attacks triggered interest in these issues in an unlikely constituency: the military and counterterrorism agencies. Some security experts noted that the countries that nurture terrorists are disproportionately those where women are marginalized."
+
+
+        ]
+
+
+}
 
 
 
 
 
-]
+
+
+
+
+
+
+
 
 
 
