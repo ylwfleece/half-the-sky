d232c95b3b72262e2a6448a41c7b4b6ef5fa19ad
Max Lettenberger <maxlettenberger@gmail.com>
2019-10-30 21:12:14 -0400

    Initialize repo with intro notes

---
+++ notes/intro_notes.js
@@ -0,0 +1,59 @@
+
+
+let introQuotes = {
+
+"pgXIV" : 
+
+[
+
+"one chinese family-planning official, Li Honggui, explained it this way: ‘If a boy gets sick, the parents may send him to the hospital at once. But if a girl gets sick, the parents may say to themselves, ‘Well, let’s see how she is tomorrow.’’The result is that as many infant girls die unnecessarily every week in China as protesters died in the one incident at Tiananmen." 
+
+,
+
+"In india, a ‘bride burning’-- to punish a woman for an inadequate dowry or to eliminate her so a man can remarry--takes place approximately once every two hours." 
+
+,
+
+"In the twin cities of islamabad and rawalpindi, Pakistan, five thousand women and girls have been doused in kerosene and set alight by family members or in-laws--or, perhaps worse, been seared with acid--for perceived disobedience just in the last nine years." 
+
+,
+
+"A tiny portion of US foreign aid is specifically targeted to women and girls." 
+
+]
+
+,
+
+"pgXV": 
+
+[
+
+"In normal circumstances women live longer than men, and so there are more females than males in much of the world." 
+
+,
+
+"China has 107 males for every 100 females in its overall population (and an even greater disproportion of newborns), India has 108, and Pakistan has 111. This has nothing to do with biology, and indeed the state of Kerala in the southwest of India, which has championed female education and equality, has the same excess of  females that exists in the United States" 
+
+,
+
+"The implication of the sex ratios, Professor Sen found, is that about 107 million females are missing from the globe today. Follow-up studies have calculated the number slightly differently, deriving alternative figures for ‘missing women’ of between 60 million and 101 million. Every year, at least another 2 million girls worldwide disappear because of gender discrimination." 
+
+,
+
+"In America, millions of women and girls face beatings or other violence from their husbands or boyfriends, and more than one in six undergoes rape or attempted rape at some point in her life, according to the National Violence Against Women survey." 
+
+,
+
+""
+
+
+
+
+
+]
+
+
+
+
+
+
